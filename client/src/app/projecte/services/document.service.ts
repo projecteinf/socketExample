9cfec40ceb020b6fd9ecd1471d32550b2d2f7009
projecteinf <projecteinf@boscdelacoma.cat>
2023-01-17 12:04:51 +0100

    Definició tipus al document.service

--- client/src/app/projecte/services/document.service.ts
+++ client/src/app/projecte/services/document.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+import { Document } from '../models/document';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
+  currentDocument:Observable<Document> = this.socket.fromEvent<Document>('document');
+  documents:Observable<string[]> = this.socket.fromEvent<string[]>('documents');
 
-  constructor() { }
+  constructor(private socket: Socket) { }
+
+  getDocument(id: string) {  this.socket.emit('getDoc', id);  }
+
+  newDocument() { this.socket.emit('addDoc', { id: this.docId(), doc: '' }); }
+
+  editDocument(document: Document) { this.socket.emit('editDoc', document);  }
+
+  private docId() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
 }
